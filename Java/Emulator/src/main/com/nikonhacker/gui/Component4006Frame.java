1f01c1f3333d9b1122f89e14383511c4421b7eee
Vicne <vicnevicne@gmail.com>
2012-01-06 01:41:15 +0100

    New component simulating Memory page 0x4006

---
+++ Java/Emulator/src/main/com/nikonhacker/gui/Component4006Frame.java
@@ -0,0 +1,27 @@
+package com.nikonhacker.gui;
+
+import com.nikonhacker.emu.memory.DebuggableMemory;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Component4006Frame extends MemoryMappedComponentFrame {
+    public Component4006Frame(String title, boolean resizable, boolean closable, boolean maximizable, boolean iconifiable, final DebuggableMemory memory, int page, EmulatorUI ui) {
+        super(title, resizable, closable, maximizable, iconifiable, memory, page, ui);
+
+        JButton addButton = new JButton("Store 0x1000 at 0x40060010");
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                memory.store16(0x40060010, 0x1000);
+            }
+        });
+        getContentPane().add(addButton);
+    }
+    
+    public void dispose() {
+        super.dispose();
+    }
+
+}
