2fb048f9396df01e817f959c0a31788c2472f9e4
Vicne <vicnevicne@gmail.com>
2012-02-08 22:47:20 +0100

    Increase heap memory for Emulator too

--- Java/Emulator/batch/startEmulator.bat
+++ Java/Emulator/batch/startEmulator.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Starting emulator UI
-java -cp "%~dp0/FrEmulator.jar";"%~dp0/lib/commons-io-2.1.jar";"%~dp0/lib/commons-lang3-3.1.jar";"%~dp0/lib/xstream-1.4.2.jar";"%~dp0/lib/jacksum.jar" com.nikonhacker.gui.EmulatorUI %*
+call java -Xmx768m -cp "%~dp0/FrEmulator.jar";"%~dp0/lib/commons-io-2.1.jar";"%~dp0/lib/commons-lang3-3.1.jar";"%~dp0/lib/xstream-1.4.2.jar";"%~dp0/lib/jacksum.jar" com.nikonhacker.gui.EmulatorUI %*
 echo Done.
 pause
