d965f18b742ef83a46dcc733e825507cf3360543
Vicne <vicnevicne@gmail.com>
2012-02-11 16:25:13 +0100

    New component to add search functionality to TextAreas

---
+++ Java/Emulator/src/main/com/nikonhacker/gui/component/SearchableTextAreaPanel.java
@@ -0,0 +1,401 @@
+package com.nikonhacker.gui.component;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * <p>This class wraps the given JTextArea inside a scrollable panel, along with a Firefox-like search bar at the bottom
+ * The search bar can be made visible or hidden at start, and features a "search term" box, forward and back buttons,
+ * and options to be case sensitive or not, and to hightlight all matches at once.</p>
+ * <p>CTRL-F opens the bar (if hidden) and performs a search based on the currently selected text of the JTextArea</p>
+ * <p>F3 searches for the next occurrence, Shift-F3 searched for the previous one</p>
+ * <p>ESC hides the bar</p>
+ * <p>Based on http://coding.derkeiler.com/Archive/Java/comp.lang.java.programmer/2008-04/msg01467.html</p>
+ * TODO : highlights should be cleared if textArea Document content changes
+ */
+public class SearchableTextAreaPanel extends JPanel implements DocumentListener, KeyListener {
+
+    // Global flag to allow fiddling with fields without triggering multiple searches
+    private boolean searchEnabled = true;
+
+    public enum SearchDirection {
+        BACKWARDS,
+        NONE,
+        FORWARD
+    }
+
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JPanel searchPanel;
+    JTextField searchTextField;
+    JCheckBox highlightAllCheckBox;
+    JCheckBox matchCaseCheckBox;
+    int currentlyHighlightedPosition = -1;
+    Map<Integer,Object> highlights = new HashMap<Integer, Object>();
+    Color defaultSearchFieldBgColor = null;
+
+    /**
+     * Create a new SearchableTextAreaPanel encapsulating the given JTextArea, with the search bar hidden
+     * @param textArea the textArea to wrap
+     */
+    public SearchableTextAreaPanel(JTextArea textArea) {
+        this(textArea, false);
+    }
+
+    /**
+     * Create a new SearchableTextAreaPanel encapsulating the given JTextArea, with the search bar optionally shown
+     * @param textArea the textArea to wrap
+     */
+    public SearchableTextAreaPanel(JTextArea textArea, boolean showBarByDefault) {
+        super(new BorderLayout());
+        this.textArea = textArea;
+        createAndShowGUI(showBarByDefault);
+    }
+
+    private void createAndShowGUI(boolean showBar) {
+        textArea.addKeyListener(this);
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
+            public void insertUpdate(DocumentEvent e) {
+                clearHighlights();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                clearHighlights();
+            }
+
+            public void changedUpdate(DocumentEvent e) {
+                clearHighlights();
+            }
+        });
+
+        scrollPane = new JScrollPane(textArea);
+        add(scrollPane, BorderLayout.CENTER);
+
+        searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        searchTextField = new JTextField(20);
+        searchTextField.addKeyListener(this);
+        searchTextField.getDocument().addDocumentListener(this);
+        defaultSearchFieldBgColor = searchTextField.getBackground();
+        searchPanel.add(searchTextField);
+
+        JButton nextButton = new JButton("Next");
+        nextButton.addKeyListener(this);
+        nextButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.FORWARD, false);
+            }
+        });
+        searchPanel.add(nextButton);
+
+        JButton previousButton = new JButton("Previous");
+        previousButton.addKeyListener(this);
+        previousButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.BACKWARDS, false);
+            }
+        });
+        searchPanel.add(previousButton);
+
+        highlightAllCheckBox = new JCheckBox("Highlight all");
+        highlightAllCheckBox.addKeyListener(this);
+        highlightAllCheckBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.NONE, true);
+            }
+        });
+        searchPanel.add(highlightAllCheckBox);
+
+        matchCaseCheckBox = new JCheckBox("Match case");
+        matchCaseCheckBox.addKeyListener(this);
+        matchCaseCheckBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.NONE, true);
+            }
+        });
+        searchPanel.add(matchCaseCheckBox);
+
+        searchPanel.setVisible(showBar); // if false, it remains hidden until CTRL-F is pressed
+        //TODO add cross icon to hide bar
+        add(searchPanel, BorderLayout.SOUTH);
+    }
+
+    /**
+     * Searches the given text with default options (case-insensitive, no highlights)
+     * @param text the text to search
+     * @return
+     */
+    public int search(String text) {
+        return search(text, false);
+    }
+
+    /**
+     * Searches the given text, optionally matching case, with the default options (no highlights)
+     * @param text the text to search
+     * @param matchCase if true, a case-sensitive search is performed
+     * @return
+     */
+    public int search(String text, boolean matchCase) {
+        return search(text, matchCase, false);
+    }
+
+    /**
+     * Searches the given text, optionally matching case, and optionally highlighting all the matches
+     * @param text the text to search
+     * @param matchCase if true, a case-sensitive search is performed
+     * @param highlightAll if true, all matches are highlighted
+     * @return
+     */
+    public int search(String text, boolean matchCase, boolean highlightAll) {
+        searchPanel.setVisible(true);
+        searchEnabled = false; // to avoid that events on field trigger lots of searches
+        searchTextField.setText(text);
+        matchCaseCheckBox.setSelected(matchCase);
+        highlightAllCheckBox.setSelected(highlightAll);
+        searchEnabled = true; // restore normal behaviour
+
+        // Perform search 
+        return performSearch(text, -1, SearchDirection.FORWARD, true);
+    }
+
+    public void clearHighlights() {
+        textArea.getHighlighter().removeAllHighlights();
+    }
+
+    private int performSearch(String searchText, int startPosition, SearchDirection direction, boolean searchTextChanged) {
+        if (searchEnabled) {
+            searchTextField.setBackground(defaultSearchFieldBgColor);
+            DefaultHighlighter.DefaultHighlightPainter nonSelectedHighlighter = new DefaultHighlighter.DefaultHighlightPainter(Color.YELLOW);
+            DefaultHighlighter.DefaultHighlightPainter selectedHighlighter = new DefaultHighlighter.DefaultHighlightPainter(Color.GREEN);
+
+            Cursor previousCursor = textArea.getCursor();
+            boolean previousEditableStatus = textArea.isEditable();
+
+            textArea.setCursor(new Cursor(Cursor.WAIT_CURSOR));
+            textArea.setEditable(false);
+
+            boolean highlightAll = highlightAllCheckBox.isSelected();
+            boolean matchCase = matchCaseCheckBox.isSelected();
+            String searchString = matchCase?searchText:searchText.toLowerCase();
+
+            Highlighter highlighter = textArea.getHighlighter();
+
+            // HighlightAll (if requested)
+            if (searchTextChanged) {
+                highlighter.removeAllHighlights();
+                if (highlightAll) {
+                    if (searchString.length() > 0) {
+                        SearchHighlighter searchHighlighter = new SearchHighlighter(textArea.getDocument(), searchString, matchCase);
+
+                        ExecutorService service = Executors.newSingleThreadExecutor();
+                        Future<java.util.List<Integer>> offsets = service.submit(searchHighlighter);
+
+                        try {
+                            for (Integer start : offsets.get()) {
+                                highlights.put(start, highlighter.addHighlight(start, start + searchString.length(), nonSelectedHighlighter));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+            // Highlight & select "current" match
+            String text = matchCase?textArea.getText():textArea.getText().toLowerCase();
+            int matchOffset = -1;
+            switch (direction) {
+                case NONE :
+                    // No move was requested. See if search still matches, just check
+                    if (matches(startPosition, searchString, text)) {
+                        matchOffset = startPosition;
+                    }
+                    else {
+                        // if check fails (text changed or match case changed), perform forward search
+                        matchOffset = text.indexOf(searchString, startPosition);
+                    }
+                    break;
+                case FORWARD:
+                    matchOffset = text.indexOf(searchString, (startPosition != -1)?startPosition + 1:0);
+                    break;
+                case BACKWARDS:
+                    matchOffset = text.lastIndexOf(searchString, (startPosition != -1)?startPosition-1:text.length());
+                    break;
+            }
+
+            // Remove previous highlight
+            Object currentHighlight = highlights.get(startPosition);
+            if (currentHighlight != null) {
+                highlighter.removeHighlight(currentHighlight);
+                if (highlightAll) {
+                    // put back a "non-current" highlight if still valid
+                    try {
+                        if (matches(startPosition, searchString, text)) {
+                            highlights.put(startPosition, highlighter.addHighlight(startPosition, startPosition + searchString.length(), nonSelectedHighlighter));
+                        }
+                    } catch (BadLocationException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            // Create new (if match found)
+            if (matchOffset > -1) {
+                setSelection(matchOffset, searchString.length());
+                try {
+                    Object oldHighlight = highlights.get(matchOffset);
+                    if (oldHighlight != null) {
+                        highlighter.removeHighlight(oldHighlight);
+                    }
+                    highlights.put(matchOffset, highlighter.addHighlight(matchOffset, matchOffset + searchString.length(), selectedHighlighter));
+                } catch (BadLocationException e) {
+                    // ignore
+                }
+            }
+            else {
+                searchTextField.setBackground(Color.PINK);
+            }
+            currentlyHighlightedPosition = matchOffset;
+
+
+            textArea.setEditable(previousEditableStatus);
+            textArea.setCursor(previousCursor);
+
+            return matchOffset;
+        }
+        return currentlyHighlightedPosition;
+    }
+
+    private void setSelection(int offset, int length) {
+        textArea.setCaretPosition(offset);
+        textArea.moveCaretPosition(offset + length);
+    }
+
+    private boolean matches(int position, String searchString, String text) {
+        if (position >= 0) {
+            if (matchCaseCheckBox.isSelected()) {
+                return text.substring(position, position + searchString.length()).equals((searchString));
+            }
+            else {
+                return text.substring(position, position + searchString.length()).equalsIgnoreCase((searchString));
+            }
+        }
+        return false;
+    }
+
+
+    // Event handlers for keypress on any component of the SearchablePanel
+
+    public void keyTyped(KeyEvent e) { /* do nothing */ }
+
+    public void keyPressed(KeyEvent e) {
+        // CTRL-F
+        if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_F) {
+            searchPanel.setVisible(true); // in case it was hidden
+            searchTextField.requestFocusInWindow();
+            searchTextField.selectAll();
+            if (textArea.getSelectedText() != null && textArea.getSelectedText().length() > 0) {
+                performSearch(textArea.getSelectedText(), textArea.getSelectionStart(), SearchDirection.NONE, true);
+                searchTextField.setText(textArea.getSelectedText());
+            }
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_F3) {
+            if (e.isShiftDown()) {
+                performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.BACKWARDS, false);
+            }
+            else {
+                performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.FORWARD, false);
+            }
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            searchPanel.setVisible(false);
+            textArea.requestFocusInWindow();
+        }
+    }
+
+    public void keyReleased(KeyEvent e) { /* do nothing */ }
+
+
+    //  Event handlers for changes to the text search field
+    
+    public void insertUpdate(DocumentEvent e) {
+        performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.NONE, true);
+    }
+
+    public void removeUpdate(DocumentEvent e) {
+        performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.NONE, true);
+    }
+
+    public void changedUpdate(DocumentEvent e) {
+        performSearch(searchTextField.getText(), currentlyHighlightedPosition, SearchDirection.NONE, true);
+    }
+
+    private static class SearchHighlighter implements Callable<java.util.List<Integer>> {
+        private Document document;
+        java.util.List<Integer> dataOffsets;
+        String searchString;
+        private boolean matchCase;
+
+        public SearchHighlighter(Document document, String searchString, boolean matchCase) {
+            this.document = document;
+            this.searchString = searchString;
+            this.matchCase = matchCase;
+        }
+
+        public java.util.List<Integer> call() throws Exception {
+            highlightSearch();
+            return dataOffsets;
+        }
+
+        private void highlightSearch() {
+            List<Integer> lineOffsets = new ArrayList<Integer>();
+            dataOffsets = new ArrayList<Integer>();
+            Element element = document.getDefaultRootElement();
+            int elementCount = element.getElementCount();
+
+            for (int i = 0; i < elementCount; i++) {
+                lineOffsets.add(element.getElement(i).getStartOffset());
+            }
+            lineOffsets.add(element.getElement(element.getElementCount() - 1).getEndOffset());
+
+            int count = 0;
+            int lsOffset;
+            int leOffset;
+
+            while (count < (lineOffsets.size() - 1)) {
+                lsOffset = lineOffsets.get(count);
+                leOffset = lineOffsets.get(count + 1);
+                count++;
+                Segment seg = new Segment();
+
+                try {
+                    document.getText(lsOffset, leOffset - lsOffset, seg);
+                } catch (BadLocationException e) {
+                    e.printStackTrace();
+                }
+
+                String line = matchCase?seg.toString():seg.toString().toLowerCase();
+                int mark = 0;
+
+                while ((mark = line.indexOf(searchString, mark)) > -1) {
+                    dataOffsets.add(lsOffset + mark);
+                    mark += searchString.length();
+                }
+            }
+        }
+    }
+}
