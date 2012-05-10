83ead9afbfe22057dfceafc4a38619ad1a6884a8
Vicne <vicnevicne@gmail.com>
2012-05-10 17:51:23 +0200

    Moved syscall generation to own class

--- Java/Emulator/src/main/com/nikonhacker/gui/component/analyse/GenerateSysSymbolsDialog.java
+++ Java/Emulator/src/main/com/nikonhacker/gui/component/analyse/GenerateSysSymbolsDialog.java
@@ -1,7 +1,7 @@
 package com.nikonhacker.gui.component.analyse;
 
 import com.nikonhacker.Format;
-import com.nikonhacker.dfr.Dfr;
+import com.nikonhacker.dfr.Syscall;
 import com.nikonhacker.emu.memory.DebuggableMemory;
 import com.nikonhacker.gui.EmulatorUI;
 import com.nikonhacker.gui.component.PrintWriterArea;
@@ -13,8 +13,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
-import java.net.URL;
-import java.util.Properties;
+import java.util.List;
 
 public class GenerateSysSymbolsDialog extends JDialog {
     private static final int INTERRUPT_VECTOR_BASE_ADDRESS = 0xDFC00;
@@ -60,29 +59,14 @@ public class GenerateSysSymbolsDialog extends JDialog {
             public void run() {
                 PrintWriter debugPrintWriter = printWriterArea.getPrintWriter();
                 try {
-                    debugPrintWriter.println("Assuming interrupt vector at 0x" + Format.asHex(INTERRUPT_VECTOR_BASE_ADDRESS, 8) + "...");
-                    int int40address = memory.load32(INTERRUPT_VECTOR_BASE_ADDRESS + 0x3FC - 0x40 * 4);
-                    debugPrintWriter.println("INT 0x40 is at 0x" + Format.asHex(int40address, 8) + "...");
-                    debugPrintWriter.println("Assuming the layout of D5100 is standard, the base address for system calls computation is stored at 0x" + Format.asHex(int40address + 64, 8) + "...");
-                    int baseAddress = memory.loadInstruction32(int40address + 64);
-                    debugPrintWriter.println("Base address is thus 0x" + Format.asHex(baseAddress, 8) + "...");
-
-                    Properties properties = new Properties() ;
-                    URL url = GenerateSysSymbolsDialog.class.getResource("realos-systemcalls.properties");
-                    //properties.load(new FileInputStream(new File(url.getFile())));
-                    properties.load(url.openStream());
-
+                    List<Syscall> syscallList = Syscall.getList(memory);
                     debugPrintWriter.println("The following lines can be pasted to a dfr.txt file :");
                     debugPrintWriter.println();
-
-                    for (Object o : properties.keySet()) {
-                        int offset = Format.parseUnsigned((String) o);
-                        String value = (String) properties.get(o);
-                        int sysFunctionAddress = baseAddress + Dfr.signExtend(16, memory.loadInstruction16(baseAddress + offset * 2));
-                        debugPrintWriter.println("-s 0x" + Format.asHex(sysFunctionAddress, 8) + "=" + value);
+                    for (Syscall syscall : syscallList) {
+                        debugPrintWriter.println("-s 0x" + Format.asHex(syscall.getAddress(), 8) + "=" + syscall.getName());
                     }
                     debugPrintWriter.println();
-                    debugPrintWriter.println("The lines above can be pasted to a dfr.txt file :");
+                    debugPrintWriter.println("The lines above can be pasted to a dfr.txt file");
                 } catch (Exception e) {
                     e.printStackTrace();
                     debugPrintWriter.println("ERROR : " + e.getClass().getName() + ": " + e.getMessage());
