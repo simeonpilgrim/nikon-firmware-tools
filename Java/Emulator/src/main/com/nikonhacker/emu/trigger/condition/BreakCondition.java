bdfcee05f7565092369f89febf21c50a71a6654b
Vicne <vicnevicne@gmail.com>
2012-05-29 23:46:30 +0200

    Added linux scripts by Eduard_S

---
+++ Java/Emulator/src/main/com/nikonhacker/emu/trigger/condition/BreakCondition.java
@@ -0,0 +1,19 @@
+package com.nikonhacker.emu.trigger.condition;
+
+import com.nikonhacker.dfr.CPUState;
+import com.nikonhacker.emu.memory.Memory;
+import com.nikonhacker.emu.trigger.BreakTrigger;
+
+/**
+ * This is an active object which basically says if a condition (or a set thereof) matches
+ * BreakTriggers are converted to a set of BreakConditions upon Emulator start
+ * @see BreakTrigger
+ */
+public interface BreakCondition {
+    /**
+     * @return the breaktrigger to which this condition belongs
+     */
+    BreakTrigger getBreakTrigger();
+
+    boolean matches(CPUState cpuState, Memory memory);
+}
