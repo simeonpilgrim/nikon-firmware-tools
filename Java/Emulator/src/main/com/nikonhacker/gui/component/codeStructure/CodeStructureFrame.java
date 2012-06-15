c03d57a0853824f514db2b58102265e51fad5542
Vicne <vicnevicne@gmail.com>
2012-06-15 19:20:23 +0200

    Show dashed lines for dynamic calls

--- Java/Emulator/src/main/com/nikonhacker/gui/component/codeStructure/CodeStructureFrame.java
+++ Java/Emulator/src/main/com/nikonhacker/gui/component/codeStructure/CodeStructureFrame.java
@@ -200,12 +200,12 @@ public class CodeStructureFrame extends DocumentFrame
             int address = i * 10;
             Function function = new Function(address, "Function" + i, "", Function.Type.STANDARD);
             codeStructure.getFunctions().put(address, function);
-            sourceFunction.getCalls().add(new Jump(0, address, null));
+            sourceFunction.getCalls().add(new Jump(0, address, null, false));
             for (int j = 1; i <= 10; i++) {
                 int address2 = i * 10 + j;
                 Function function2 = new Function(address2, "SubFunction" + j, "", Function.Type.STANDARD);
                 codeStructure.getFunctions().put(address2, function2);
-                function.getCalls().add(new Jump(address, address2, null));
+                function.getCalls().add(new Jump(address, address2, null, false));
             }
         }
     }
