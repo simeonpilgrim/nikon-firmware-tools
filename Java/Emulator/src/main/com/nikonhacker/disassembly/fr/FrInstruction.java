960e3cb58ac08fd28dd3e0007cad744f49616540
Vicne <vicnevicne@gmail.com>
2012-08-05 00:22:07 +0200

    (minor)

--- Java/Emulator/src/main/com/nikonhacker/disassembly/fr/FrInstruction.java
+++ Java/Emulator/src/main/com/nikonhacker/disassembly/fr/FrInstruction.java
@@ -392,6 +392,8 @@ public class FrInstruction extends Instruction {
      * @param name the symbolic name
      * @param displayFormat a string specifying how to format operands. It is a list of characters among :<br/>
 <pre>
+; separates the operand part and the comment part<br/>
+<br/>
 2 : constant operand (x) must be multiplied by 2 (e.g. address of 16-bit data)<br/>
 4 : constant operand (x) must be multiplied by 4 (e.g. address of 32-bit data)<br/>
 r : constant operand (x) is a relative address<br/>
@@ -401,7 +403,7 @@ b : shift2 (?)<br/>
 x : set bit #8 of x to indicate that register bitmap (x) used by this operation must be reversed
 y : add 8 to c to mark that register bitmap (x) used by this operation represents R8-R15 and not R0-R7
 <br/>
-# ( ) + , - ; @ are copied as is<br/>
+# ( ) + , - @ are copied as is<br/>
 & : outputs a ,<br/>
 <br/>
 s : outputs x as a signed hex<br/>
