040ca15502fd36d6f08b7731530a5f1d20e2730a
Purdea Andrei <andrei@purdea.ro>
2012-09-03 01:17:01 +0300

    Changing scripts, because the filename of the .jar file changed.

--- Java/Emulator/batch/startDfr.bat
+++ Java/Emulator/batch/startDfr.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Starting Dfr
-call java -Xmx512m -cp "%~dp0/FrEmulator.jar";"%~dp0/lib/commons-io-2.1.jar";"%~dp0/lib/commons-lang3-3.1.jar";"%~dp0/lib/xstream-1.4.2.jar";"%~dp0/lib/jacksum.jar" com.nikonhacker.dfr.Dfr %*
+call java -Xmx512m -cp "%~dp0/NikonEmulator.jar";"%~dp0/lib/commons-io-2.1.jar";"%~dp0/lib/commons-lang3-3.1.jar";"%~dp0/lib/xstream-1.4.2.jar";"%~dp0/lib/jacksum.jar" com.nikonhacker.dfr.Dfr %*
 echo Done.
 pause
