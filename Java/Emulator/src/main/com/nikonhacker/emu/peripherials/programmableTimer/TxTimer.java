0e22171f40ce38b2863b69893101c14e321585ca
Vicne <vicnevicne@gmail.com>
2012-12-02 15:41:00 +0100

    Fixed timerNumber overriding

--- Java/Emulator/src/main/com/nikonhacker/emu/peripherials/programmableTimer/TxTimer.java
+++ Java/Emulator/src/main/com/nikonhacker/emu/peripherials/programmableTimer/TxTimer.java
@@ -11,7 +11,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class TxTimer extends ProgrammableTimer implements CpuPowerModeChangeListener {
-    int timerNumber;
     private TxCPUState cpuState;
     private TxClockGenerator clockGenerator;
 
