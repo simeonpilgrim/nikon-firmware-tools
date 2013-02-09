3c697d5250a7cb4c64eb2e400622514e65c508b4
Vicne <vicnevicne@gmail.com>
2013-02-09 16:54:55 +0100

    Define constants for all alt register names

--- Java/Emulator/src/main/com/nikonhacker/disassembly/tx/TxCPUState.java
+++ Java/Emulator/src/main/com/nikonhacker/disassembly/tx/TxCPUState.java
@@ -92,9 +92,33 @@ public class TxCPUState extends CPUState {
             }
     };
 
+    public final static int AT = 1;
+    public final static int V0 = 2;
+    public final static int V1 = 3;
+    public final static int A0 = 4;
+    public final static int A1 = 5;
+    public final static int A2 = 6;
+    public final static int A3 = 7;
+    public final static int T0 = 8;
+    public final static int T1 = 9;
+    public final static int T2 = 10;
+    public final static int T3 = 11;
+    public final static int T4 = 12;
+    public final static int T5 = 13;
+    public final static int T6 = 14;
+    public final static int T7 = 15;
     public final static int S0 = 16;
     public final static int S1 = 17;
+    public final static int S2 = 18;
+    public final static int S3 = 19;
+    public final static int S4 = 20;
+    public final static int S5 = 21;
+    public final static int S6 = 22;
+    public final static int S7 = 23;
     public final static int T8 = 24;
+    public final static int T9 = 25;
+    public final static int K0 = 26;
+    public final static int K1 = 27;
     public final static int GP = 28;
     public final static int SP = 29;
     public final static int FP = 30;
