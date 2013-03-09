bebd6f07383e7f49507f814aebdd59b515b3f76d
Vicne <vicnevicne@gmail.com>
2013-03-09 14:01:45 +0100

    Testing...

--- Java/Emulator/src/main/com/nikonhacker/emu/peripherials/serialInterface/eeprom/St950x0.java
+++ Java/Emulator/src/main/com/nikonhacker/emu/peripherials/serialInterface/eeprom/St950x0.java
@@ -4,9 +4,6 @@ import com.nikonhacker.Format;
 import com.nikonhacker.emu.peripherials.serialInterface.DummySerialDevice;
 import com.nikonhacker.emu.peripherials.serialInterface.SerialDevice;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * This implementation is based on the ST95040 datasheet at
  * http://www.st.com/st-web-ui/static/active/en/resource/technical/document/datasheet/CD00001755.pdf
@@ -19,10 +16,10 @@ public class St950x0 implements SerialDevice {
     public enum Command {WREN, WRDI, RDSR, WRSR, READ0, READ1, WRITE0, WRITE1}
 
     private Command command;
-    private Integer writeAddress, readAddress;
-    private boolean clockEnabled;
+    private Integer writeAddress = 0, readAddress = 0;
+    private boolean commandComplete = true;
 
-    private Queue<Integer> outputQueue = new LinkedList<Integer>();
+    private boolean clockEnabled;
 
     int statusRegister = 0b1111_0000;
     private byte[] memory = new byte[512];
@@ -30,81 +27,85 @@ public class St950x0 implements SerialDevice {
     public St950x0() {
         this.name = this.getClass().getName();
     }
+
     public St950x0(String name) {
         this.name = name;
     }
 
     public Integer read() {
         if (command == Command.RDSR) {
-            command = null; // RDSR can only return one byte
-            return statusRegister;
+            commandComplete = true;
+            return statusRegister; // will be returned over and over until next command
         }
         else if (command == Command.READ0 || command == Command.READ1) {
             int value = memory[readAddress];
-            // Prepare next read by incrementing address
-            readAddress++;
-            if (readAddress % 16 == 0) {
-                // We've reached a new page. Wrap
-                readAddress -= 16;
-            }
+            // Prepare next read by incrementing address, wrapping at 16
+            readAddress = (readAddress + 1) & 0xF;
             return value;
         }
         else {
-            System.err.println("Attempt to read from " + this.getClass().getName() + " while no READ command has been issued");
-            return null;
+            System.err.println("Unknown state, returning 0x00");
+            return 0x00;
         }
     }
 
     public void write(Integer value) {
+        // Writing a value to serial eeprom means clock is ticking, so a value has to be transmitted back synchronously
+        connectedDevice.write(read());
+
         if (value == null) {
             System.out.println("St950x0.write(null)");
         }
         else {
-            if (!outputQueue.isEmpty()) {
-                // If clock ticks while there are values waiting, use the clock to output value
-                connectedDevice.write(outputQueue.poll());
-            }
-            if (command == null) {
+            if (commandComplete) {
                 // first byte is a new command
                 switch (value) {
                     case 0b0000_0110:
                         // WREN Set Write Enable Latch
                         // TODO
+                        commandComplete = false;
                         break;
                     case 0b0000_0100:
                         // WRDI Reset Write Enable Latch
                         // TODO
+                        commandComplete = false;
                         break;
                     case 0b0000_0101:
                         // RDSR Read Status Register
-                        outputQueue.add(statusRegister);
+                        command = Command.RDSR;
+                        commandComplete = false;
                         break;
                     case 0b0000_0001:
                         // WRSR Write Status Register
                         // TODO
+                        commandComplete = false;
                         break;
                     case 0b0000_0011:
                         // READ Read Data from Memory Array (0)
                         command = Command.READ0;
+                        commandComplete = false;
                         break;
                     case 0b0000_1011:
                         // READ Read Data from Memory Array (1)
                         command = Command.READ1;
+                        commandComplete = false;
                         break;
                     case 0b0000_0010:
                         // WRITE Write Data to Memory Array (0)
                         command = Command.WRITE0;
+                        commandComplete = false;
                         break;
                     case 0b0000_1010:
                         // WRITE Write Data to Memory Array (1)
                         command = Command.WRITE1;
+                        commandComplete = false;
                         break;
                     default:
                         System.err.println("Unknown command : 0b" + Format.asBinary(value, 8));
                 }
             }
             else {
-                // Command READ or WRITE .
+                // Handle READ or WRITE
                 if (writeAddress == null) {
                     // "decode" 2nd byte as an address
                     if (command == Command.READ0) {
