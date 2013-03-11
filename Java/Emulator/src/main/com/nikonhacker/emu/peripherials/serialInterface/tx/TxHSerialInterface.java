1947d265111e2cbd221d95991db1577065508516
Vicne <vicnevicne@gmail.com>
2013-03-11 21:10:22 +0100

    Refined FIFO interruption thresholds (assuming error in spec)

--- Java/Emulator/src/main/com/nikonhacker/emu/peripherials/serialInterface/tx/TxHSerialInterface.java
+++ Java/Emulator/src/main/com/nikonhacker/emu/peripherials/serialInterface/tx/TxHSerialInterface.java
@@ -41,7 +41,7 @@ public class TxHSerialInterface extends TxSerialInterface {
      */
     @Override
     protected void computeRxFillLevel() {
-        rxInterruptFillLevel = rfc & 0b11111; // 5 bits, independent of Half/Full duplex
+        rxInterruptFillLevel = rfc & 0b11111; // RIL is on 5 bits, independent of Half/Full duplex
         if (rxInterruptFillLevel == 0) {
             // Special case
             rxInterruptFillLevel = 32;
@@ -54,7 +54,13 @@ public class TxHSerialInterface extends TxSerialInterface {
      */
     @Override
     protected void computeTxFillLevel() {
-        txInterruptFillLevel = tfc & 0b111111; // 6 bits, independent of Half/Full duplex
+        // According to the specification, TIL is on 6 bits, independent of Half/Full duplex
+        // txInterruptFillLevel = tfc & 0b111111; // 6 bits
+
+        // However, the code writes 0xA0 to tfc for 2 bytes sent, which would mean 32 (not really meaningul).
+        // Moreover, RIL is 5 bits, and the example at page 15-14, it says "HSC0TFC <5:0> = 00000" with 5 zeroes...
+        // So let's consider it is 5 bits
+        txInterruptFillLevel = tfc & 0b11111; // 5 bits, independent of Half/Full duplex
         if (txInterruptFillLevel > 32) {
             throw new RuntimeException(getName() + " error : HSC0TFC<TIL5:1> is more than 32 (" + txInterruptFillLevel + ")");
         }
