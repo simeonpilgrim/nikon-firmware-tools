c84c96b62e999872f4ba951ad6667d652a1e06e8
Vicne <vicnevicne@gmail.com>
2013-03-31 20:29:14 +0200

    Basic A/D register interfacing

--- Java/Emulator/src/main/com/nikonhacker/emu/peripherials/adConverter/tx/TxAdUnit.java
+++ Java/Emulator/src/main/com/nikonhacker/emu/peripherials/adConverter/tx/TxAdUnit.java
@@ -1,39 +1,58 @@
 package com.nikonhacker.emu.peripherials.adConverter.tx;
 
 public class TxAdUnit {
-    private char unitName;
+    private final char unitName;
+    private final int numChannels;
+
+    private int clk;
 
     private int mod0;
     private int mod1;
     private int mod2;
     private int mod3;
     private int mod4;
+    private int mod5;
 
     private int reg[];
 
     private int regSp;
-    private final int nbChannels;
 
-    public TxAdUnit(char unitName, int nbChannels) {
+    private int comReg0;
+    private int comReg1;
+    private boolean isEoc = false, isBusy = false;
+    private boolean isPrioEoc = false, isPrioBusy = false;
+    public TxAdUnit(char unitName, int numChannels) {
         this.unitName = unitName;
-        this.nbChannels = nbChannels;
-        reg = new int[nbChannels];
+        this.numChannels = numChannels;
+        reg = new int[numChannels];
     }
 
     public char getUnitName() {
         return unitName;
     }
 
-    public int getNbChannels() {
-        return nbChannels;
+    public int getNumChannels() {
+        return numChannels;
+    }
+
+    public int getClk() {
+        return clk;
+    }
+
+    public void setClk(int clk) {
+        this.clk = clk;
     }
 
     public int getMod0() {
-        return mod0;
+        return  (isEoc?0b10000000:0) | (isBusy?0b01000000:0) | (mod0 & 0b00011110);
     }
 
     public void setMod0(int mod0) {
-        this.mod0 = mod0;
+        this.mod0 = mod0 & 0b00011111;
+        if ((mod0 & 0b1) != 0) {
+            System.out.println(toString() + " started");
+            //TODO start conversion
+        }
     }
 
     public int getMod1() {
@@ -45,27 +64,39 @@ public class TxAdUnit {
     }
 
     public int getMod2() {
-        return mod2;
+        return (isPrioEoc?0b10000000:0) | (isPrioBusy?0b01000000:0) | (mod2 & 0b00111111);
     }
 
     public void setMod2(int mod2) {
-        this.mod2 = mod2;
+        this.mod2 = mod2 & 0b0011111;
     }
 
     public int getMod3() {
-        return mod3;
+        return mod3 & 0b00111111;
     }
 
     public void setMod3(int mod3) {
-        this.mod3 = mod3;
+        this.mod3 = mod3 & 0b10111111;
     }
 
     public int getMod4() {
-        return mod4;
+        return mod4 & 0b11110000;
     }
 
     public void setMod4(int mod4) {
-        this.mod4 = mod4;
+        // if ADRST goes from 10 to 01, perform a Software reset
+        if ((this.mod4 & 0b11) == 0b10 && (mod4 & 0b11)== 0b01) {
+            // TODO reset
+        }
+        this.mod4 = mod4 & 0b11110011;
+    }
+
+    public int getMod5() {
+        return mod5 & 0b00111111;
+    }
+
+    public void setMod5(int mod5) {
+        this.mod5 = mod5 & 0b10111111;
     }
 
     public int getReg(int regNumber) {
@@ -82,5 +113,26 @@ public class TxAdUnit {
 
     public void setRegSp(int regSp) {
         this.regSp = regSp;
+    }
+
+    public int getComReg0() {
+        return comReg0;
+    }
+
+    public void setComReg0(int comReg0) {
+        this.comReg0 = comReg0;
+    }
+
+    public int getComReg1() {
+        return comReg1;
+    }
+
+    public void setComReg1(int comReg1) {
+        this.comReg1 = comReg1;
+    }
+
+    @Override
+    public String toString() {
+        return "A/D Unit " + unitName;
     }
 }
