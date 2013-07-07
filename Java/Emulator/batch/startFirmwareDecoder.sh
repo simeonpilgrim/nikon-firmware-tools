5e35463fd3bb19e7637ef3f3fe3d539ea7a973c9
Vicne <vicnevicne@gmail.com>
2013-07-07 21:25:27 +0200

    Added "./" as . is not always in the PATH

--- Java/Emulator/batch/startFirmwareDecoder.sh
+++ Java/Emulator/batch/startFirmwareDecoder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo "Starting FirmwareDecoder"
-java -cp "NikonEmulator.jar":"lib/commons-io-2.1.jar":"lib/commons-lang3-3.1.jar":"lib/xstream-1.4.2.jar":"lib/jacksum.jar" com.nikonhacker.encoding.FirmwareDecoder "$@"
+java -cp "./NikonEmulator.jar":"./lib/commons-io-2.1.jar":"./lib/commons-lang3-3.1.jar":"./lib/xstream-1.4.2.jar":"./lib/jacksum.jar" com.nikonhacker.encoding.FirmwareDecoder "$@"
 
