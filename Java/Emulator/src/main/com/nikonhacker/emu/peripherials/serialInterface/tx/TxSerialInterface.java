e2028756c4a0da284c53bb79625e6dad5e8d0f5e
Vicne <vicnevicne@gmail.com>
2013-08-15 23:13:52 +0200

    Now syncing on MasterClock instead of Emulator cycles

--- Java/Emulator/src/main/com/nikonhacker/emu/peripherials/serialInterface/tx/TxSerialInterface.java
+++ Java/Emulator/src/main/com/nikonhacker/emu/peripherials/serialInterface/tx/TxSerialInterface.java
@@ -2,9 +2,10 @@ package com.nikonhacker.emu.peripherials.serialInterface.tx;
 
 import com.nikonhacker.Constants;
 import com.nikonhacker.Format;
-import com.nikonhacker.emu.CycleCounterListener;
+import com.nikonhacker.emu.Clockable;
 import com.nikonhacker.emu.Emulator;
 import com.nikonhacker.emu.Platform;
+import com.nikonhacker.emu.peripherials.clock.tx.TxClockGenerator;
 import com.nikonhacker.emu.peripherials.interruptController.tx.TxInterruptController;
 import com.nikonhacker.emu.peripherials.serialInterface.SerialInterface;
 
@@ -14,9 +15,34 @@ import java.util.Queue;
 /**
  * Behaviour is Based on Toshiba documentation TMP19A44F10XBG_TMP19A44FEXBG_en_datasheet_100401.pdf
  */
-public class TxSerialInterface extends SerialInterface implements CycleCounterListener {
+public class TxSerialInterface extends SerialInterface implements Clockable {
     private static final int SERIAL_RX_FIFO_SIZE = 4;
-    private static final int CYCLES_PER_BYTE = 500;
+
+    public static final int EN_SIOE_MASK = 0b00000001;
+
+    private static final int CR_IOC_MASK  = 0b00000001;
+    private static final int CR_FERR_MASK = 0b00000100;
+    private static final int CR_PERR_MASK = 0b00001000;
+    private static final int CR_OERR_MASK = 0b00010000;
+
+    private static final int MOD0_SC_MASK   = 0b00000011;
+    private static final int MOD0_SM_MASK   = 0b00001100;
+    private static final int MOD0_WU_MASK   = 0b00010000;
+    private static final int MOD0_RXE_MASK  = 0b00100000;
+    private static final int MOD0_CTSE_MASK = 0b01000000;
+    private static final int MOD0_TB8_MASK  = 0b10000000;
+
+    private static final int MOD1_SINT_MASK    = 0b00001110;
+    private static final int MOD1_TXE_MASK     = 0b00010000;
+    private static final int MOD1_FDPX_MASK    = 0b01100000;
+    private static final int MOD1_FDPX_RX_MASK = 0b00100000;
+    private static final int MOD1_FDPX_TX_MASK = 0b01000000;
+
+    private static final int BRCR_BRS_MASK    = 0b00001111;
+    private static final int BRCR_BRCK_MASK   = 0b00110000;
+    private static final int BRCR_BRADDE_MASK = 0b01000000;
+
+    private static final int BRADD_BRK_MASK   = 0b00001111;
 
     /**
      * Rx buffer
@@ -53,14 +79,12 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
     protected int tst = 0b10000000; // Transmit FIFO status register
     protected int fcnf; // FIFO configuration register
 
-    private Integer cycleCounter = 0;
-    private Integer delayedValue;
-
     public TxSerialInterface(int serialInterfaceNumber, Platform platform, Emulator emulator, boolean logSerialMessages) {
         // TODO: syncing on emulator should be replaced by a sync on masterclock
         // TODO: interruptController and emulator(now masterclock) should be replaced by platform
         super(serialInterfaceNumber, platform, emulator, logSerialMessages);
     }
+
 
     public int getEn() {
         return en;
@@ -68,8 +92,9 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
 
     public void setEn(int en) {
 //        System.out.println(getName() + ".setEn(0x" + Format.asHex(en, 8) + ")");
-        this.en = en & 0x1;
+        this.en = en & EN_SIOE_MASK;
     }
+
 
     /**
      * This is the method called by assembly code to read data received via serial port
@@ -119,7 +144,7 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
             }
             if (isMod1TxeSet()) {
                 // Insert delay of a few CPU cycles.
-                emulator.addCycleCounterListener(this);
+                platform.getMasterClock().add(this);
             }
         }
         else {
@@ -127,6 +152,7 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
             txBuf = buf;
         }
     }
+
 
     public int getCr() {
         // TODO in UART mode, if parity or 9-bit communication, set corresponding bits according to the value in buf or FIFO head!
@@ -138,14 +164,12 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
     }
 
     public void setCr(int cr) {
-//        if (getName().equals("HSerial #2") && cr == 0x14) {
-//            throw new RuntimeException("Setting HSC2CR to " + Format.asHex(cr,8)) ;
-//        }
 //        System.out.println(getName() + ".setCr(0x" + Format.asHex(cr, 8) + ")");
 
         // RB8 and error flags are not writable
         this.cr = (this.cr & 0b10011100) | (cr & 0b01100011);
     }
+
 
     public int getMod0() {
         return mod0;
@@ -158,6 +182,7 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
             if (logSerialMessages) System.err.println(getName() + " is being configured as UART. Only I/O serial mode is supported for now");
         }
     }
+
 
     public int getMod1() {
         return mod1;
@@ -179,10 +204,11 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
             // Signal if there are values waiting
             if (getNbTxValuesWaiting() > 0) {
                 // Insert delay of a few CPU cycles.
-                emulator.addCycleCounterListener(this);
+                platform.getMasterClock().add(this);
             }
         }
     }
+
 
     public int getMod2() {
         return mod2;
@@ -207,6 +233,7 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
         this.mod2 = (this.mod2 & 0b11100000) | (mod2 & 0b00011111);
     }
 
+
     public int getBrcr() {
         return brcr;
     }
@@ -215,6 +242,7 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
 //        System.out.println(getName() + ".setBrcr(0x" + Format.asHex(brcr, 8) + ")");
         this.brcr = brcr;
     }
+
 
     public int getBradd() {
         return bradd;
@@ -280,7 +308,6 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
     }
 
 
-
     public void setRst(int rst) {
         throw new RuntimeException(getName() + " RST register should not be written");
     }
@@ -308,89 +335,134 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
      * @return true if Enabled
      */
     protected boolean isEnSet() {
-        return (en != 0);
+        return (en & EN_SIOE_MASK) != 0;
     }
 
+
     /*
-     * @return true if I/O interface mode is in SCLK input clock mode
+     * @return true if I/O interface mode is in SCLK input clock mode (slave)
      */
-    protected boolean isCrIocSet() {
-        return (cr & 0b00000001) != 0;
+    public boolean isCrIocSet() {
+        return (cr & CR_IOC_MASK) != 0;
     }
 
     /**
      * Set CR Overrun error flag
      */
     protected void setCrOerr() {
-        cr = cr | 0b00010000;
+        cr = cr | CR_OERR_MASK;
     }
 
     /**
      * Clear CR Overrun error flag
      */
     protected void clearCrOerr() {
-        cr = cr & 0b11101111;
+        cr = cr & ~CR_OERR_MASK;
     }
 
     /**
      * Set CR Parity/Underrun error flag
      */
     protected void setCrPerr() {
-        cr = cr | 0b00001000;
+        cr = cr | CR_PERR_MASK;
     }
 
     /**
      * Clear CR Parity/Underrun error flag
      */
     protected void clearCrPerr() {
-        cr = cr & 0b11110111;
+        cr = cr & ~CR_PERR_MASK;
     }
 
     /**
      * Set CR Framing error flag
      */
     protected void setCrFerr() {
-        cr = cr | 0b00000100;
+        cr = cr | CR_FERR_MASK;
     }
 
     /**
      * Clear CR Framing error flag
      */
     protected void clearCrFerr() {
-        cr = cr & 0b11111011;
+        cr = cr & ~CR_FERR_MASK;
     }
 
 
-    protected boolean isMod0RxeSet() {
-        return (mod0 & 0b00100000) != 0;
-    }
-
-    protected void clearMod0Rxe() {
-        mod0 = mod0 & 0b11011111;
+    protected int getMod0Sc() {
+        return mod0 & MOD0_SC_MASK;
     }
 
     protected int getMod0Sm() {
-        return (mod0 >> 2) & 0b11;
+        return (mod0 & MOD0_SM_MASK) >> 2;
+    }
+
+    protected boolean isMod0WuSet() {
+        return (mod0 & MOD0_WU_MASK) != 0;
+    }
+
+    protected boolean isMod0RxeSet() {
+        return (mod0 & MOD0_RXE_MASK) != 0;
+    }
+
+    protected void clearMod0Rxe() {
+        mod0 = mod0 & ~MOD0_RXE_MASK;
+    }
+
+    protected boolean isMod0CtseSet() {
+        return (mod0 & MOD0_CTSE_MASK) != 0;
+    }
+
+    protected boolean isMod0Tb8Set() {
+        return (mod0 & MOD0_TB8_MASK) != 0;
+    }
+
+    protected boolean isIoMode() {
+        return getMod0Sm() == 0b00;
+    }
+
+
+    protected int getMod1Sint(){
+        return (mod1 & MOD1_SINT_MASK) >> 1;
     }
 
     protected boolean isMod1TxeSet() {
-        return (mod1 & 0b00010000) != 0;
+        return (mod1 & MOD1_TXE_MASK) != 0;
     }
 
     protected void clearMod1Txe() {
-        mod1 = mod1 & 0b11101111;
+        mod1 = mod1 & ~MOD1_TXE_MASK;
     }
 
     protected int getMod1Fdpx() {
-        return (mod1 >> 5) & 0b11;
+        return (mod1 & MOD1_FDPX_MASK) >> 5;
     }
 
     protected boolean isMod1FdpxRxSet() {
-        return (mod1 & 0b00100000) != 0;
+        return (mod1 & MOD1_FDPX_RX_MASK) != 0;
     }
 
     protected boolean isMod1FdpxTxSet() {
-        return (mod1 & 0b01000000) != 0;
+        return (mod1 & MOD1_FDPX_TX_MASK) != 0;
+    }
+
+    private boolean isFullDuplex() {
+        return getMod1Fdpx() == 0b11;
+    }
+
+    protected int getIntervalTimeInSclk() {
+        switch (getMod1Sint()) {
+            case 0b000: return 0;
+            case 0b001: return 1;
+            case 0b010: return 2;
+            case 0b011: return 4;
+            case 0b100: return 8;
+            case 0b101: return 16;
+            case 0b110: return 32;
+            case 0b111: return 64;
+            default:
+                throw new RuntimeException("Error in TxSerialInterface.getIntervalTimeInSclk()");
+        }
     }
 
 
@@ -458,6 +530,23 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
     }
 
 
+    public int getBrcrBrs() {
+        return (brcr & BRCR_BRS_MASK);
+    }
+
+    public int getBrcrBrck() {
+        return (brcr & BRCR_BRCK_MASK) >> 4;
+    }
+
+    public boolean isBrcrBraddeSet() {
+        return (brcr & BRCR_BRADDE_MASK) != 0;
+    }
+
+
+    public int getBraddBrk() {
+        return (brcr & BRADD_BRK_MASK);
+    }
+
 
     protected boolean isFcnfRfstSet() {
         return (fcnf & 0b00010000) != 0;
@@ -480,12 +569,102 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
     }
 
 
+    // Clock computation
+    // See block diagram and details at section 14.2
+
+    public int getSioClk() {
+        if (isIoMode()) {
+            // I/O interface mode, clock is specified in the control register SC0CR
+            if (isCrIocSet()) {
+                // SCLK input
+                return 0; // We are slave
+            }
+            else {
+                // SCLK output
+                return getBaudrate() / 2;
+            }
+        }
+        else {
+            // UART mode, clock is specified in the mode control register0 (SC0MOD0<SC1:0>)
+            switch (getMod0Sc()) {
+                case 0b00: // Timer TB0OUT (from TMRB0)
+                    return 0; // TODO
+                case 0b01: // Baud rate generator
+                    return getBaudrate();
+                case 0b10: // Internal fSYS/2 clock
+                    return ((TxClockGenerator)platform.getClockGenerator()).getFsysHz() / 2;
+                case 0b11: // External clock (SCLK0 input)
+                    return 0; // TODO Clock input
+                default:
+                    throw new RuntimeException("Error: TxSerialInterface.getClockHz");
+            }
+        }
+    }
+
+    /**
+     *  See section 14.4.1 of the specification
+     */
+    public int getBaudrate() {
+        double divider;
+        int n = getDivideRatio();
+        if (isBrcrBraddeSet()) {
+            // N + ((16 - K) / 16) division
+            if (isIoMode()) {
+                // I/O interface mode
+                throw new RuntimeException("Error: TxSerialInterface.getBaudrate: BRADDE cannot be enabled in I/O mode");
+            }
+            if (n == 1 || n == 16) {
+                throw new RuntimeException("Error: TxSerialInterface.getBaudrate: BRADDE cannot be enabled with N=" + n);
+            }
+            int k = getBraddBrk();
+            if (k == 0) {
+                throw new RuntimeException("Error: TxSerialInterface.getBaudrate: K=0");
+            }
+            divider = n + (16-k)/16.0;
+        }
+        else {
+            // N division
+            divider = n;
+        }
+        return (int) (getBaudrateInputClock() / divider);
+    }
+
+
+    /**
+     * The baud rate generator uses one of four clocks (?T1, ?T4, ?T16 or ?T64)
+     * supplied from the prescaler output clock
+     */
+    private int getBaudrateInputClock() {
+        switch (getBrcrBrck()) {
+            case 0b00:
+                return ((TxClockGenerator)platform.getClockGenerator()).getFt0Hz()/2; // T1 = T0 / 2
+            case 0b01:
+                return ((TxClockGenerator)platform.getClockGenerator()).getFt0Hz()/8; // T4 = T0 / 8
+            case 0b10:
+                return ((TxClockGenerator)platform.getClockGenerator()).getFt0Hz()/32; // T16 = T0 / 32
+            case 0b11:
+                return ((TxClockGenerator)platform.getClockGenerator()).getFt0Hz()/128; // T64 = T0 / 128
+            default:
+                throw new RuntimeException("Error: TxSerialInterface.getBaudrate: BRCR:BRCK=" + getBrcrBrck());
+        }
+    }
+
+    public int getDivideRatio() {
+        int brs = getBrcrBrs();
+        if (brs == 0b0000) {
+            return 16;
+        }
+        else {
+            return brs;
+        }
+    }
+
     /**
      * Compute Rx FIFO Fill Level to generate interrupts
      * Note: overridden in TxHSerialInterface
      */
     protected void computeRxFillLevel() {
-        if (getMod1Fdpx() == 0b11) {
+        if (isFullDuplex()) {
             // Full duplex
             rxInterruptFillLevel = rfc & 0b1;
             if (rxInterruptFillLevel == 0) {
@@ -508,7 +687,7 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
      * Note: overridden in TxHSerialInterface
      */
     protected void computeTxFillLevel() {
-        if (getMod1Fdpx() == 0b11) {
+        if (isFullDuplex()) {
             // Full duplex
             txInterruptFillLevel = tfc & 0b1;
         }
@@ -519,9 +698,8 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
     }
 
 
-
     protected int getMaxFifoSize() {
-        if (getMod1Fdpx() == 0b11) {
+        if (isFullDuplex()) {
             return SERIAL_RX_FIFO_SIZE / 2;
         }
         else {
@@ -705,31 +883,43 @@ public class TxSerialInterface extends SerialInterface implements CycleCounterLi
         return getName();
     }
 
+
     @Override
+    public int getChip() {
+        return Constants.CHIP_TX;
+    }
+
+    @Override
+    public int getFrequencyHz() {
+        // dataRate  = 1/time_for_one_data
+        //           = 1/(time_for_n_bits + interval)
+        //           = 1/(n * time_for_1_bit + interval)
+        //           = 1/(time_for_1_bit * (n + interval_in_SCLK))
+        //           = (1/time_for_1_bit) / (n + interval_in_SCLK))
+        //           = bitrate / (n + interval_in_SCLK))
+        return getSioClk() / (getNumBits() + getIntervalTimeInSclk());
+    }
+
+
     /**
-     * The goal of this is to delay the actual write to the other device of CYCLES_PER_BYTE cycles
-     * The TX empty interrupt occurs at half way
+     * The goal of this is to delay the actual transfer according to the currently selected baud rate
      */
-    public boolean onCycleCountChange(long oldCount, int increment) {
-        boolean remainRegistered = true;
-        if (cycleCounter > CYCLES_PER_BYTE/2 && delayedValue == null) {
-            delayedValue = read();
-        }
-        cycleCounter += increment;
-        if (cycleCounter >= CYCLES_PER_BYTE) {
-            if (delayedValue != null) {
-                super.valueReady(delayedValue);
-                // device may stop transmission automatically if FIFO was used and configured like this
-                if (!isMod1TxeSet())
-                    remainRegistered = false;
+    @Override
+    public Object onClockTick() throws Exception {
+        Integer value = read();
+        if (value != null) {
+            super.valueReady(value);
+            // device may stop transmission automatically if FIFO was used and configured like this
+            if (!isMod1TxeSet()) {
+                // unregister
+                return "DONE";
             }
-            else {
-                // End of transmission - unregister
-                remainRegistered = false;
-            }
-            delayedValue = null;
-            cycleCounter = 0;
         }
-        return remainRegistered;
+        else {
+            // End of transmission - unregister
+            return "DONE";
+        }
+        // Remain registered
+        return null;
     }
 }
