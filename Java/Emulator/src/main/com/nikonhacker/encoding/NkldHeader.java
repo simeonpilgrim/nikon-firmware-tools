e973da739cbcfed879f62da26962109bcd981e8d
coderat <coderat@gmx.de>
2013-11-01 03:28:43 +0100

    Add NKLD data decoder

---
+++ Java/Emulator/src/main/com/nikonhacker/encoding/NkldHeader.java
@@ -0,0 +1,15 @@
+package com.nikonhacker.encoding;
+
+public class NkldHeader {
+
+    /* offset of data block */
+    public int dataOffset;
+    public int totalLength;
+    public int majorVersion;
+    public int minorVersion;
+    public int entryCount;
+    public int magic;
+    public int dataLength;
+    public int reserved;
+    public int crcPadding;
+}
