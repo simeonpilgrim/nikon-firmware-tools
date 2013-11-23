bbfa3cd728cc73dd7d5685622445991d76821089
coderat <coderat@gmx.de>
2013-11-23 18:45:35 +0100

    Added lens emulation prototype

--- Java/Emulator/src/main/com/nikonhacker/gui/component/serialInterface/SerialPanelFactory.java
+++ Java/Emulator/src/main/com/nikonhacker/gui/component/serialInterface/SerialPanelFactory.java
@@ -3,9 +3,11 @@ package com.nikonhacker.gui.component.serialInterface;
 import com.nikonhacker.emu.peripherials.serialInterface.SerialDevice;
 import com.nikonhacker.emu.peripherials.serialInterface.eeprom.St950x0;
 import com.nikonhacker.emu.peripherials.serialInterface.lcd.LcdDriver;
+import com.nikonhacker.emu.peripherials.serialInterface.fMount.FMountCircuit;
 import com.nikonhacker.gui.EmulatorUI;
 import com.nikonhacker.gui.component.serialInterface.eeprom.EepromSerialPanel;
 import com.nikonhacker.gui.component.serialInterface.viewfinderLcd.LcdSerialPanel;
+import com.nikonhacker.gui.component.serialInterface.fMount.FMountSerialPanel;
 
 public class SerialPanelFactory {
     public static SerialDevicePanel getSerialDevicePanel(SerialDevice serialDevice, EmulatorUI ui) {
@@ -14,6 +16,9 @@ public class SerialPanelFactory {
         }
         else if (serialDevice instanceof LcdDriver) {
             return new LcdSerialPanel((LcdDriver)serialDevice, ui);
+        }
+        else if (serialDevice instanceof FMountCircuit) {
+            return new FMountSerialPanel((FMountCircuit)serialDevice, ui);
         }
         else return new RxTxSerialPanel(serialDevice);
     }
