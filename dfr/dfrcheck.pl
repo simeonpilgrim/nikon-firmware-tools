310b95ee0473034544f707649958b75acbfd9c3d
coderat <coderat@gmx.de>
2014-06-02 21:50:21 +0200

    Extended dfrcheck utility

--- dfr/dfrcheck.pl
+++ dfr/dfrcheck.pl
@@ -13,6 +13,7 @@
 # 15Feb14 coderat: add -s option for printing all symbols sorted
 # 10May14 coderat: add -n option to find new addresses
 # 19May14 coderat: performance optimisation
+# 20May14 coderat: function parameters check
 #
 
 my @iranges;
@@ -37,6 +38,87 @@ sub testRange (\@ \@ $) {
 			printf ("!!! %s range %08X-%08X conflicts with range %08X-%08X\n", $name, $$range[0], $$range[1], $$old_range[0], $$old_range[1]);
 		}
 	}
+}
+
+# ------------------------------------------------------------------------
+
+sub parsePrototype($) {
+		my $str = $_[0];
+
+		$str =~ s#^\s*([0-9A-Za-z_:.@\$\?]+)##;
+		my $name = $1;
+
+		# [spaces](...)[spaces][\r]
+		$str =~ s#^\s*##;
+		if (length($str)>0) {
+
+			if (ord($str)==ord('(')) {
+				#start of arguments
+
+				if ($str =~ m#\((.*)\)\s*$#) {
+					$str = $1;
+
+					# remove comments
+					$str =~ s#/\*.*\*/##gm;
+
+					# [spaces]
+					$str =~ s#^\s*##;
+
+					while (length($str)>0) {
+
+						# reg '['...']'[,]
+						# reg : Rn | $an | $vn | $sn
+
+						if ($str =~ s#^\$*[A-Za-z]\d+\s*\[##) {
+
+							# OUT name
+							# IN name[; OUT name]
+							while (1) {
+
+								if (!($str =~ s#^\s*(in|out)\s+##i)) {
+									print "!!! Wrong or missing parameter type in $name\n";
+									return $name;
+								}
+
+								# name[spaces]
+								if (!($str =~ s#^[0-9A-Za-z_:.@\$\?]+\s*##)) {
+									print "!!! Missing parameter name in $name\n";
+									return $name;
+								}
+
+								if (ord($str)==ord(';')) {
+									$str = substr ($str,1);
+									next;
+
+								} elsif (ord($str)==ord(']')) {
+									$str =~ s#^]\s*##;
+									last;
+								}
+							}
+
+						} else {
+							print "!!! Incorrect parameter in $name\n";
+							last;
+						}
+
+						if (length($str)==0) {
+							last;
+						}
+
+						if (!($str =~ s#^,\s*##)) {
+							print "!!! Missing coma in parameters of $name\n";
+							last;
+						}
+					}
+				} else {
+					print "!!! Missing ) in $name\n";
+				}
+
+			} else {
+				print "!!! Missing ( in $name\n";
+			}
+		}
+		return $name;
 }
 
 # ------------------------------------------------------------------------
@@ -81,23 +163,19 @@ sub testConsistency {
 
 		# find all symbols
 		while ($buf =~ m#\-s[ ]+0x([0-9a-fA-F]+)=([^\r\n]+)#gm) {
-			my ($addr,$name) = (hex($1),$2);
+			my ($addr,$prototype) = (hex($1),$2);
 
 			if (exists($Symbols{$addr}) ) {
-				print "!!! Symbol address $name conflicts with ".$Symbols{$addr}."\n";
+				print "!!! Symbol address $prototype conflicts with ".$Symbols{$addr}."\n";
 			}
 
-			my $strippedName = $name;
-	#		my $i = index($name,'(');
-	#		if ($ i!= -1) {
-	#		}
-			$strippedName =~ s#\(.+##;
+			my $name = parsePrototype($prototype);
 
-			if (exists($reversedSymbols{$strippedName}) ) {
-				print "!!! Symbol name duplicated $strippedName\n";
+			if (exists($reversedSymbols{$name}) ) {
+				print "!!! Symbol name duplicated $name\n";
 			}
-			$Symbols{$addr} = $name;
-			$reversedSymbols{$strippedName} = $addr;
+			$Symbols{$addr} = $prototype;
+			$reversedSymbols{$name} = $addr;
 		}
 	}
 	close IN;
