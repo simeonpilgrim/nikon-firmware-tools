a13742b82f9604b6d09ebcffe0f5ab01183e5e6d
Simeon Pilgrim <simeon@simeonpilgrim.com>
2014-08-24 12:34:08 +1200

    Change some D600 Patches to released, and Add 64Mbps beta patch

--- Nikon-Patch/Nikon-Patch/D600.cs
+++ Nikon-Patch/Nikon-Patch/D600.cs
@@ -64,16 +64,35 @@ namespace Nikon_Patch
             new Patch(1, 0x23BE0, new byte[] { 0x00, 0x98, 0x96, 0x80 } , new byte[] { 0x01, 0x31, 0x2D, 0x00 } ),
                                   };
 
+        Patch[] patch_1080_64mbps_NQ = {
+            new Patch(1, 0x23B4E, Sys.mbps24 , Sys.mbps64 ),
+            new Patch(1, 0x23B54, Sys.mbps20 , Sys.mbps60 ),
+            new Patch(1, 0x23B62, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23B68, Sys.mbps10 , Sys.mbps20 ),
+
+            new Patch(1, 0x23B7A, Sys.mbps24 , Sys.mbps64 ),
+            new Patch(1, 0x23B80, Sys.mbps20 , Sys.mbps60 ),
+            new Patch(1, 0x23B8E, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23B94, Sys.mbps10 , Sys.mbps20 ),
+
+            new Patch(1, 0x23BC6, Sys.mbps24 , Sys.mbps64 ),
+            new Patch(1, 0x23BCC, Sys.mbps20 , Sys.mbps60 ),
+            new Patch(1, 0x23BDA, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23BE0, Sys.mbps10 , Sys.mbps20 ),
+        };
+        
+        
         public D600_0101()
         {
             p = new Package();
             Model = "D600";
             Version = "1.01";
 
-            Patches.Add(new PatchSet(PatchLevel.Released, "Video 1080 HQ 36mbps Bit-rate", patch_1080_36mbps, patch_1080_54mbps, patch_1080_36mbps_NQ, patch_1080_54mbps_NQ));
-            Patches.Add(new PatchSet(PatchLevel.Released, "Video 1080 HQ 54mbps Bit-rate", patch_1080_54mbps, patch_1080_36mbps, patch_1080_36mbps_NQ, patch_1080_54mbps_NQ));
-            Patches.Add(new PatchSet(PatchLevel.Beta, "Video 1080 HQ 36mbps Bit-rate NQ old HQ", patch_1080_36mbps_NQ, patch_1080_36mbps, patch_1080_54mbps, patch_1080_54mbps_NQ));
-            Patches.Add(new PatchSet(PatchLevel.Beta, "Video 1080 HQ 54mbps Bit-rate NQ old HQ", patch_1080_54mbps_NQ, patch_1080_36mbps, patch_1080_54mbps, patch_1080_36mbps_NQ));
+            Patches.Add(new PatchSet(PatchLevel.Released, "Video 1080 HQ 36mbps Bit-rate", patch_1080_36mbps, patch_1080_54mbps, patch_1080_36mbps_NQ, patch_1080_54mbps_NQ, patch_1080_64mbps_NQ));
+            Patches.Add(new PatchSet(PatchLevel.Released, "Video 1080 HQ 54mbps Bit-rate", patch_1080_54mbps, patch_1080_36mbps, patch_1080_36mbps_NQ, patch_1080_54mbps_NQ, patch_1080_64mbps_NQ));
+            Patches.Add(new PatchSet(PatchLevel.Released, "Video 1080 HQ 36mbps Bit-rate NQ old HQ", patch_1080_36mbps_NQ, patch_1080_36mbps, patch_1080_54mbps, patch_1080_54mbps_NQ, patch_1080_64mbps_NQ));
+            Patches.Add(new PatchSet(PatchLevel.Released, "Video 1080 HQ 54mbps Bit-rate NQ old HQ", patch_1080_54mbps_NQ, patch_1080_36mbps, patch_1080_54mbps, patch_1080_36mbps_NQ, patch_1080_64mbps_NQ));
+            Patches.Add(new PatchSet(PatchLevel.Beta, "Video 1080 HQ 64mbps Bit-rate NQ old HQ", patch_1080_64mbps_NQ, patch_1080_36mbps, patch_1080_54mbps, patch_1080_36mbps_NQ, patch_1080_54mbps_NQ));
         }
     }
 }
