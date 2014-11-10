d6789f567b9095028c51c2efa11c7cdcd85f4dd8
Simeon Pilgrim <simeon@simeonpilgrim.com>
2014-11-11 10:49:13 +1300

    D7000 v1.03 - Full screen HDMI 720p

--- Nikon-Patch/Nikon-Patch/D7000.cs
+++ Nikon-Patch/Nikon-Patch/D7000.cs
@@ -199,12 +199,21 @@ namespace Nikon_Patch
             new Patch(1, 0x199E17, new byte[] {0x90, 0x00, 0x00 }, new byte[] {0x25, 0xC2, 0xD4}),
         };
 
+       Patch[] hdmi_full_sized = {
+            // 720
+            new Patch(1, 0x1E6C34, new byte[] {0x00,0x00,0x5A,0xA4, 0x00,0x0E,0x3D,0x52, 0x00,0x15,0x45,0x52}, new byte[] {0x00,0x00,0x00,0x00, 0x00,0x0E,0x10,0x00, 0x00,0x15,0x18,0x00} ),
+            new Patch(1, 0x1E6C40, Sys.LittleWords(1280,640,952,634,476,634 ), Sys.LittleWords(1280,640,1280,720,640,720 )),
+                                };
+
         public D7000_0103()
         {
             p = new Package();
             Model = "D7000";
             Version = "1.03";
 
+            Patches.Add(new PatchSet(PatchLevel.Released, "HDMI full sized", hdmi_full_sized));
+
+          
             Patches.Add(new PatchSet(PatchLevel.Released, "Remove Time Based Video Restrictions", patch_1));
             //Patches.Add(new PatchSet(false, "Test Live View Record", patch_2));
             Patches.Add(new PatchSet(PatchLevel.Released, "Disable Nikon Star Eater", patch_stareater));
