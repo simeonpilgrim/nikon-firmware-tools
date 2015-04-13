588f2d06ce6fbf4be8ba7a3b7f434d92f3ed8bf6
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2015-04-13 17:43:09 +1200

    refactor MakePatch to handle binary blobs of patch as well as install binaries

--- Firmware Patch/MakePatch/MakePatch.cs
+++ Firmware Patch/MakePatch/MakePatch.cs
@@ -27,11 +27,33 @@ namespace MakePatch
 
             if (res == false)
             {
+                res = BlobDiff(args[0], args[1]);
+            }
+
+            if (res == false)
+            {
                 Usage();
                 Console.WriteLine("Firmware files ether not valid, or not the same versions");
                 Console.WriteLine();
                 return;
             }
+        }
+
+        private static bool BlobDiff(string file_a, string file_b)
+        {
+            var br_a = new BinaryReader(File.OpenRead(file_a));
+            var br_b = new BinaryReader(File.OpenRead(file_b));
+
+            if( br_a.BaseStream.Length != br_b.BaseStream.Length )
+                return false;
+  
+            int len = (int)br_a.BaseStream.Length;
+            using (var sw = new StreamWriter(file_b + "_patch.txt"))
+            {
+                CompareBlock(br_a.ReadBytes(len), br_b.ReadBytes(len), 0, len, sw);
+            }
+
+            return true;
         }
 
         private static bool PackageDiff(string file_a, string file_b)
@@ -40,7 +62,7 @@ namespace MakePatch
             var b = new Package();
             if (a.TryOpen(file_a) && b.TryOpen(file_b))
             {
-                if ( a.header.Count != b.header.Count ) 
+                if (a.header.Count != b.header.Count)
                     return false;
 
                 int count = a.header.Count;
@@ -48,44 +70,48 @@ namespace MakePatch
                 {
                     if (a.header[i].Item1 != b.header[i].Item1 || // file name
                         a.header[i].Item2 != b.header[i].Item2 || // file offset
-                        a.header[i].Item3 != b.header[i].Item3 )  // file length
+                        a.header[i].Item3 != b.header[i].Item3)  // file length
                         return false;
                 }
 
                 using (var sw = new StreamWriter(file_b + "_patch.txt"))
                 {
-                    List<Tuple<byte, byte>> chunk = new List<Tuple<byte, byte>>();
-
                     for (int i = 0; i < count; i++)
                     {
                         var ba = a.blocks[i];
                         var bb = b.blocks[i];
-
                         var dataend = ba.Length - 2; // ignore CRC at end of block
-                        int last = -1;
-                        for (int j = 0; j < dataend; j++)
-                        {
-                            if (ba[j] != bb[j])
-                            {
-                                if (j != last + 1)
-                                {
-                                    OutputChunk(sw, i, chunk, last);
-                                    chunk.Clear();
-                                }
 
-                                chunk.Add(new Tuple<byte, byte>(ba[j], bb[j]));
-                                last = j;
-                            }
-                        }
-                        OutputChunk(sw, i, chunk, last);
-                        chunk.Clear();
+                        CompareBlock(ba, bb, i, dataend, sw);
                     }
-
                 }
 
+                return true;
             }
 
             return false;
+        }
+
+        private static void CompareBlock(byte[] ba, byte[] bb, int blockId, int dataend, StreamWriter sw)
+        {
+            List<Tuple<byte, byte>> chunk = new List<Tuple<byte, byte>>();
+
+            int last = -1;
+            for (int j = 0; j < dataend; j++)
+            {
+                if (ba[j] != bb[j])
+                {
+                    if (j != last + 1)
+                    {
+                        OutputChunk(sw, blockId, chunk, last);
+                        chunk.Clear();
+                    }
+
+                    chunk.Add(new Tuple<byte, byte>(ba[j], bb[j]));
+                    last = j;
+                }
+            }
+            OutputChunk(sw, blockId, chunk, last);
         }
 
         private static void OutputChunk(StreamWriter sw, int fileId, List<Tuple<byte, byte>> chunk, int last)
