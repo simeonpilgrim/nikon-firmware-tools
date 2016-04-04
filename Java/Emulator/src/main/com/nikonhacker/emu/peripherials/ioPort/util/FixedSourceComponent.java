9008751f962d22c6f1c0b6e95b1fce498b94e27c
vicnevicne <vicnevicne@gmail.com>
2016-04-04 21:07:28 +0200

    (comment)

--- Java/Emulator/src/main/com/nikonhacker/emu/peripherials/ioPort/util/FixedSourceComponent.java
+++ Java/Emulator/src/main/com/nikonhacker/emu/peripherials/ioPort/util/FixedSourceComponent.java
@@ -3,6 +3,7 @@ package com.nikonhacker.emu.peripherials.ioPort.util;
 /**
  * This component, when inserted at pin A, acts as a source that always outputs the value given in the constructor
  * The pin formerly attached to A is attached to pin 2 of this component, but is left dangling
+ * Although, keeping this pin 2 allows to retain what was previously connected, and restore it if this "override" if removed
  */
 public class FixedSourceComponent extends Abstract2PinComponent {
     public FixedSourceComponent(int overridingValue, String name, boolean logPinMessages) {
