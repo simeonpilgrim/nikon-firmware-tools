ddd29562e4a7c2ee5e8fb57790377b411dccebc5
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2017-12-29 22:59:58 +1300

    drop unused build command, as I have one that works, so the non-working onces are not needed..

--- Nikon-Patch-JS/src/nikon_patch/build.cmd
+++ Nikon-Patch-JS/src/nikon_patch/build.cmd
@@ -1,2 +1,1 @@
-REM emcc nikon_patch.c -o nikon_patch.js -s WASM=1 -s EXPORTED_FUNCTIONS="['_main','_detectFirmware']" -s "BINARYEN_METHOD='native-wasm'" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall','cwrap']" -O2 -s NO_EXIT_RUNTIME=1
 emcc nikon_patch.c md5.c md5driver.c xor.c -o nikon_patch.js -s WASM=1 -O3 -s TOTAL_MEMORY=73400320 -s EXTRA_EXPORTED_RUNTIME_METHODS="['getValue']"
