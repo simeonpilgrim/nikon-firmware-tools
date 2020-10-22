77cc3d2fdeac4997205880ba4b4e39e1503bd34b
davidgfb <davidgracia99@gmail.com>
2020-10-22 21:56:32 +0200

    Change import java.io.OutputStream.*; to multiple individual libraries
    Change import java.io.OutputStream.*; to multiple individual libraries

--- Java/Emulator/src/main/com/nikonhacker/XStreamUtils.java
+++ Java/Emulator/src/main/com/nikonhacker/XStreamUtils.java
@@ -4,7 +4,13 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.StaxDriver;
 import com.thoughtworks.xstream.mapper.MapperWrapper;
 
-import java.io.*;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.Charset;
 
 public class XStreamUtils {
