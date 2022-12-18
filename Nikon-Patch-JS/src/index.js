7c4f97ccfab43e38ea68faa6b6deadc3cf5eac20
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2022-12-18 16:46:40 +1300

    nikon-patch-js: bump npm and node versions (locally) and then move to react 18

--- Nikon-Patch-JS/src/index.js
+++ Nikon-Patch-JS/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 //registerServiceWorker();
