13785ae193f55d538bb2084f8b17b47fb3c66ac3
ECS-tasy <729117569@qq.com>
2026-01-15 17:59:48 +0800

    fix(D610): Add missing 1080p30fps entries & Enable bitrate patches for V1.04
    - Fixed an issue where D610 v1.01 bitrate patches (36Mbps & 54Mbps) were missing the offsets for 1080p 30fps recording.
    - Synchronized the patch offset lists with the 64Mbps patch to ensure consistent coverage across all frame rates (24p/25p/30p).
    - Enabled Video Bitrate patches for D610 Firmware V1.04 (offsets verified against V1.01).
    - Verified offsets against unpacked firmware binary.

--- Nikon-Patch/LocalUI/D610.cs
+++ Nikon-Patch/LocalUI/D610.cs
@@ -3,37 +3,91 @@
     class D610_0101 : Firmware
     {
         Patch[] patch_1080_36mbps_NQ = {
+            // 1080 0 24 fps
             new Patch(1, 0x23DD2, Sys.mbps24 , Sys.mbps40 ),
             new Patch(1, 0x23DD8, Sys.mbps20 , Sys.mbps36 ),
             new Patch(1, 0x23DE6, Sys.mbps12 , Sys.mbps24 ),
             new Patch(1, 0x23DEC, Sys.mbps10 , Sys.mbps20 ),
 
+            // 1080 2 30 fps
+            new Patch(1, 0x23DFE, Sys.mbps24 , Sys.mbps40 ),
+            new Patch(1, 0x23E04, Sys.mbps20 , Sys.mbps36 ),
+            new Patch(1, 0x23E12, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23E18, Sys.mbps10 , Sys.mbps20 ),
+
+            // 1080 6 25 fps
             new Patch(1, 0x23E4A, Sys.mbps24 , Sys.mbps40 ),
             new Patch(1, 0x23E50, Sys.mbps20 , Sys.mbps36 ),
             new Patch(1, 0x23E5E, Sys.mbps12 , Sys.mbps24 ),
             new Patch(1, 0x23E64, Sys.mbps10 , Sys.mbps20 ),
 
+            // 720 0 24 fps
+            new Patch(1, 0x23EB0, Sys.mbps10, Sys.mbps40 ),
+            new Patch(1, 0x23EB6, Sys.mbps8 , Sys.mbps36 ),
+            new Patch(1, 0x23EC4, Sys.mbps6 , Sys.mbps24 ),
+            new Patch(1, 0x23ECA, Sys.mbps5 , Sys.mbps20 ),
+
+            // 720 1 60 fps
             new Patch(1, 0x23EDC, Sys.mbps24 , Sys.mbps40 ),
             new Patch(1, 0x23EE2, Sys.mbps20 , Sys.mbps36 ),
             new Patch(1, 0x23EF0, Sys.mbps12 , Sys.mbps24 ),
             new Patch(1, 0x23EF6, Sys.mbps10 , Sys.mbps20 ),
+
+            // 720 2 30 fps
+            new Patch(1, 0x23F08, Sys.mbps12, Sys.mbps40 ),
+            new Patch(1, 0x23F0E, Sys.mbps10, Sys.mbps36 ),
+            new Patch(1, 0x23F1C, Sys.mbps8 , Sys.mbps24 ),
+            new Patch(1, 0x23F22, Sys.mbps6 , Sys.mbps20 ),
+
+            // 720 5 50 fps
+            new Patch(1, 0x23F54, Sys.mbps24 , Sys.mbps40 ),
+            new Patch(1, 0x23F5A, Sys.mbps20 , Sys.mbps36 ),
+            new Patch(1, 0x23F68, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23F6E, Sys.mbps10 , Sys.mbps20 ),
         };
 
         Patch[] patch_1080_54mbps_NQ = {
+            // 1080 0 24 fps
             new Patch(1, 0x23DD2, Sys.mbps24 , Sys.mbps57 ),
             new Patch(1, 0x23DD8, Sys.mbps20 , Sys.mbps53 ),
             new Patch(1, 0x23DE6, Sys.mbps12 , Sys.mbps24 ),
             new Patch(1, 0x23DEC, Sys.mbps10 , Sys.mbps20 ),
 
+            // 1080 2 30 fps
+            new Patch(1, 0x23DFE, Sys.mbps24 , Sys.mbps57 ),
+            new Patch(1, 0x23E04, Sys.mbps20 , Sys.mbps53 ),
+            new Patch(1, 0x23E12, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23E18, Sys.mbps10 , Sys.mbps20 ),
+
+            // 1080 6 25 fps
             new Patch(1, 0x23E4A, Sys.mbps24 , Sys.mbps57 ),
             new Patch(1, 0x23E50, Sys.mbps20 , Sys.mbps53 ),
             new Patch(1, 0x23E5E, Sys.mbps12 , Sys.mbps24 ),
             new Patch(1, 0x23E64, Sys.mbps10 , Sys.mbps20 ),
 
+            // 720 0 24 fps
+            new Patch(1, 0x23EB0, Sys.mbps10, Sys.mbps57 ),
+            new Patch(1, 0x23EB6, Sys.mbps8 , Sys.mbps53 ),
+            new Patch(1, 0x23EC4, Sys.mbps6 , Sys.mbps24 ),
+            new Patch(1, 0x23ECA, Sys.mbps5 , Sys.mbps20 ),
+
+            // 720 1 60 fps
             new Patch(1, 0x23EDC, Sys.mbps24 , Sys.mbps57 ),
             new Patch(1, 0x23EE2, Sys.mbps20 , Sys.mbps53 ),
             new Patch(1, 0x23EF0, Sys.mbps12 , Sys.mbps24 ),
             new Patch(1, 0x23EF6, Sys.mbps10 , Sys.mbps20 ),
+
+            // 720 2 30 fps
+            new Patch(1, 0x23F08, Sys.mbps12, Sys.mbps57 ),
+            new Patch(1, 0x23F0E, Sys.mbps10, Sys.mbps53 ),
+            new Patch(1, 0x23F1C, Sys.mbps8 , Sys.mbps24 ),
+            new Patch(1, 0x23F22, Sys.mbps6 , Sys.mbps20 ),
+
+            // 720 5 50 fps
+            new Patch(1, 0x23F54, Sys.mbps24 , Sys.mbps57 ),
+            new Patch(1, 0x23F5A, Sys.mbps20 , Sys.mbps53 ),
+            new Patch(1, 0x23F68, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23F6E, Sys.mbps10 , Sys.mbps20 ),
         };
 
 
@@ -136,6 +190,140 @@
             new Patch(1, 0x41EC5C, new byte[] { 0xE2, 0x06 }, new byte[] { 0xE0, 0x06 }),
         };
 
+        Patch[] patch_1080_36mbps_NQ = {
+            // 1080 0 24 fps
+            new Patch(1, 0x23DD2, Sys.mbps24 , Sys.mbps40 ),
+            new Patch(1, 0x23DD8, Sys.mbps20 , Sys.mbps36 ),
+            new Patch(1, 0x23DE6, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23DEC, Sys.mbps10 , Sys.mbps20 ),
+
+            // 1080 2 30 fps
+            new Patch(1, 0x23DFE, Sys.mbps24 , Sys.mbps40 ),
+            new Patch(1, 0x23E04, Sys.mbps20 , Sys.mbps36 ),
+            new Patch(1, 0x23E12, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23E18, Sys.mbps10 , Sys.mbps20 ),
+
+            // 1080 6 25 fps
+            new Patch(1, 0x23E4A, Sys.mbps24 , Sys.mbps40 ),
+            new Patch(1, 0x23E50, Sys.mbps20 , Sys.mbps36 ),
+            new Patch(1, 0x23E5E, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23E64, Sys.mbps10 , Sys.mbps20 ),
+
+            // 720 0 24 fps
+            new Patch(1, 0x23EB0, Sys.mbps10, Sys.mbps40 ),
+            new Patch(1, 0x23EB6, Sys.mbps8 , Sys.mbps36 ),
+            new Patch(1, 0x23EC4, Sys.mbps6 , Sys.mbps24 ),
+            new Patch(1, 0x23ECA, Sys.mbps5 , Sys.mbps20 ),
+
+            // 720 1 60 fps
+            new Patch(1, 0x23EDC, Sys.mbps24 , Sys.mbps40 ),
+            new Patch(1, 0x23EE2, Sys.mbps20 , Sys.mbps36 ),
+            new Patch(1, 0x23EF0, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23EF6, Sys.mbps10 , Sys.mbps20 ),
+
+            // 720 2 30 fps
+            new Patch(1, 0x23F08, Sys.mbps12, Sys.mbps40 ),
+            new Patch(1, 0x23F0E, Sys.mbps10, Sys.mbps36 ),
+            new Patch(1, 0x23F1C, Sys.mbps8 , Sys.mbps24 ),
+            new Patch(1, 0x23F22, Sys.mbps6 , Sys.mbps20 ),
+
+            // 720 5 50 fps
+            new Patch(1, 0x23F54, Sys.mbps24 , Sys.mbps40 ),
+            new Patch(1, 0x23F5A, Sys.mbps20 , Sys.mbps36 ),
+            new Patch(1, 0x23F68, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23F6E, Sys.mbps10 , Sys.mbps20 ),
+        };
+
+        Patch[] patch_1080_54mbps_NQ = {
+            // 1080 0 24 fps
+            new Patch(1, 0x23DD2, Sys.mbps24 , Sys.mbps57 ),
+            new Patch(1, 0x23DD8, Sys.mbps20 , Sys.mbps53 ),
+            new Patch(1, 0x23DE6, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23DEC, Sys.mbps10 , Sys.mbps20 ),
+
+            // 1080 2 30 fps
+            new Patch(1, 0x23DFE, Sys.mbps24 , Sys.mbps57 ),
+            new Patch(1, 0x23E04, Sys.mbps20 , Sys.mbps53 ),
+            new Patch(1, 0x23E12, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23E18, Sys.mbps10 , Sys.mbps20 ),
+
+            // 1080 6 25 fps
+            new Patch(1, 0x23E4A, Sys.mbps24 , Sys.mbps57 ),
+            new Patch(1, 0x23E50, Sys.mbps20 , Sys.mbps53 ),
+            new Patch(1, 0x23E5E, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23E64, Sys.mbps10 , Sys.mbps20 ),
+
+            // 720 0 24 fps
+            new Patch(1, 0x23EB0, Sys.mbps10, Sys.mbps57 ),
+            new Patch(1, 0x23EB6, Sys.mbps8 , Sys.mbps53 ),
+            new Patch(1, 0x23EC4, Sys.mbps6 , Sys.mbps24 ),
+            new Patch(1, 0x23ECA, Sys.mbps5 , Sys.mbps20 ),
+
+            // 720 1 60 fps
+            new Patch(1, 0x23EDC, Sys.mbps24 , Sys.mbps57 ),
+            new Patch(1, 0x23EE2, Sys.mbps20 , Sys.mbps53 ),
+            new Patch(1, 0x23EF0, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23EF6, Sys.mbps10 , Sys.mbps20 ),
+
+            // 720 2 30 fps
+            new Patch(1, 0x23F08, Sys.mbps12, Sys.mbps57 ),
+            new Patch(1, 0x23F0E, Sys.mbps10, Sys.mbps53 ),
+            new Patch(1, 0x23F1C, Sys.mbps8 , Sys.mbps24 ),
+            new Patch(1, 0x23F22, Sys.mbps6 , Sys.mbps20 ),
+
+            // 720 5 50 fps
+            new Patch(1, 0x23F54, Sys.mbps24 , Sys.mbps57 ),
+            new Patch(1, 0x23F5A, Sys.mbps20 , Sys.mbps53 ),
+            new Patch(1, 0x23F68, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23F6E, Sys.mbps10 , Sys.mbps20 ),
+        };
+
+        Patch[] patch_1080_720_64mbps_NQ = {
+            // 1080 0 24 fps
+            new Patch(1, 0x23DD2, Sys.mbps24 , Sys.mbps64 ),
+            new Patch(1, 0x23DD8, Sys.mbps20 , Sys.mbps60 ),
+            new Patch(1, 0x23DE6, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23DEC, Sys.mbps10 , Sys.mbps20 ),
+
+            // 1080 2 30 fps
+            new Patch(1, 0x23DFE, Sys.mbps24 , Sys.mbps64 ),
+            new Patch(1, 0x23E04, Sys.mbps20 , Sys.mbps60 ),
+            new Patch(1, 0x23E12, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23E18, Sys.mbps10 , Sys.mbps20 ),
+
+            // 1080 6 25 fps
+            new Patch(1, 0x23E4A, Sys.mbps24 , Sys.mbps64 ),
+            new Patch(1, 0x23E50, Sys.mbps20 , Sys.mbps60 ),
+            new Patch(1, 0x23E5E, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23E64, Sys.mbps10 , Sys.mbps20 ),
+
+            
+            // 720 0 24 fps
+            new Patch(1, 0x23EB0, Sys.mbps10, Sys.mbps64 ),
+            new Patch(1, 0x23EB6, Sys.mbps8 , Sys.mbps60 ),
+            new Patch(1, 0x23EC4, Sys.mbps6 , Sys.mbps24 ),
+            new Patch(1, 0x23ECA, Sys.mbps5 , Sys.mbps20 ),
+
+            // 720 1 60 fps
+            new Patch(1, 0x23EDC, Sys.mbps24 , Sys.mbps64 ),
+            new Patch(1, 0x23EE2, Sys.mbps20 , Sys.mbps60 ),
+            new Patch(1, 0x23EF0, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23EF6, Sys.mbps10 , Sys.mbps20 ),
+
+            // 720 2 30 fps
+            new Patch(1, 0x23F08, Sys.mbps12, Sys.mbps64 ),
+            new Patch(1, 0x23F0E, Sys.mbps10, Sys.mbps60 ),
+            new Patch(1, 0x23F1C, Sys.mbps8 , Sys.mbps24 ),
+            new Patch(1, 0x23F22, Sys.mbps6 , Sys.mbps20 ),
+
+            // 720 5 50 fps
+            new Patch(1, 0x23F54, Sys.mbps24 , Sys.mbps64 ),
+            new Patch(1, 0x23F5A, Sys.mbps20 , Sys.mbps60 ),
+            new Patch(1, 0x23F68, Sys.mbps12 , Sys.mbps24 ),
+            new Patch(1, 0x23F6E, Sys.mbps10 , Sys.mbps20 ),
+
+                                       };
+
         public D610_0104()
         {
             p = new Package();
@@ -143,6 +331,11 @@
             Version = "1.04";
 
             Patches.Add(new PatchSet(PatchLevel.Beta, "Multi-Language Support", patch_Language_Fix));
+            
+            // Added bitrate patches (offsets verified to be identical to v1.01)
+            Patches.Add(new PatchSet(PatchLevel.Beta, "Video 1080 HQ 36mbps Bit-rate NQ old HQ", patch_1080_36mbps_NQ, patch_1080_54mbps_NQ, patch_1080_720_64mbps_NQ));
+            Patches.Add(new PatchSet(PatchLevel.Beta, "Video 1080 HQ 54mbps Bit-rate NQ old HQ", patch_1080_54mbps_NQ, patch_1080_36mbps_NQ, patch_1080_720_64mbps_NQ));
+            Patches.Add(new PatchSet(PatchLevel.Beta, "Video 1080/720 HQ 64mbps Bit-rate NQ old HQ", patch_1080_720_64mbps_NQ, patch_1080_36mbps_NQ, patch_1080_54mbps_NQ));
         }
     }
 }
